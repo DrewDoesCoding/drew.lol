5b2111a58e4281111b1c62344735d1ac85b0637f
DrewDoesCoding <drew@drew.lol>
2024-03-27 01:38:34 -0400

    Added Schema.org

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['nuxt-gtag'], 
-  devtools: { enabled: true }
+  modules: ['nuxt-gtag', 'nuxt-schema-org'], 
+  devtools: { enabled: true },
+  site: {
+    url: 'https://drew.lol',
+    name: 'Drew',
+  }
 })
